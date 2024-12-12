984e2e8fac6cce974f93faa43d20a93f5175ad37
wsafight <984292420@qq.com>
2024-12-12 14:07:39 +0800

    feat: change deploy action

--- astro.config.mjs
+++ astro.config.mjs
@@ -11,7 +11,7 @@ export default defineConfig({
   },
   integrations: [
     starlight({
-      title: "手撸解释器教程",
+      title: "手写解释器",
       social: {
         github: "https://github.com/wsafight/craftinginterpreters-zh",
       },
